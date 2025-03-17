62ee03d50f9b70c5b5ca6192688768b7d523e556
Marcos Abreu <marcosabreudasilva@gmail.com>
2025-03-17 19:29:15 -0300

    fix: production adaptation

--- app/js/helpers/suggestions.js
+++ app/js/helpers/suggestions.js
@@ -1,329 +1,333 @@
-export default class suggestions{
-    constructor(){
-        this.list = [
-        {
-            "name": "Lemon Juice", 
-            "type": "Natural Juice", 
-            "category": "Group 1", 
-            "src": "../assets/images/lemon-juice.jpg",
-            "recipe": {
-                "ingredients": ["1 Lemon","1 Liter of Water","3 Soup Spoons of Sugar"],
-                "steps": ["Squeeze the lemon","Add the lemon juice to watter","Mix the sugar to mixture"]
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 5
+export default class suggestions {
+  constructor() {
+    this.list = [
+      {
+        name: "Lemon Juice",
+        type: "Natural Juice",
+        category: "Group 1",
+        src: "./assets/images/lemon-juice.jpg",
+        recipe: {
+          ingredients: ["1 Lemon", "1 Liter of Water", "3 Soup Spoons of Sugar"],
+          steps: ["Squeeze the lemon", "Add the lemon juice to watter", "Mix the sugar to mixture"],
         },
-        {
-            "name": "Papaya Yogurt", 
-            "type": "Natural Yogurt", 
-            "category": "Group 1", 
-            "src": "../assets/images/papaya-yogurt.jpg",
-            "recipe": {
-                "ingredients": ["1/2 Papaya","1 cup of Natural Yogurt"],
-                "steps": ["Peel the papaya and cut in cubes","Add the Papaya cubes and the Yogurt to the mixer","Mix until don't see any pieces of Papaya"]
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 10
+        amount: 1,
+        measurement: "Cup",
+        preparation: 5,
+      },
+      {
+        name: "Papaya Yogurt",
+        type: "Natural Yogurt",
+        category: "Group 1",
+        src: "./assets/images/papaya-yogurt.jpg",
+        recipe: {
+          ingredients: ["1/2 Papaya", "1 cup of Natural Yogurt"],
+          steps: [
+            "Peel the papaya and cut in cubes",
+            "Add the Papaya cubes and the Yogurt to the mixer",
+            "Mix until don't see any pieces of Papaya",
+          ],
         },
-        {
-            "name": "Coconut Yogurt", 
-            "type": "Natural Yogurt", 
-            "category": "Group 1", 
-            "src": "../assets/images/coconut-yogurt.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 20
+        amount: 1,
+        measurement: "Cup",
+        preparation: 10,
+      },
+      {
+        name: "Coconut Yogurt",
+        type: "Natural Yogurt",
+        category: "Group 1",
+        src: "./assets/images/coconut-yogurt.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Banana Yogurt", 
-            "type": "Natural Yogurt", 
-            "category": "Group 1", 
-            "src": "../assets/images/banana-yogurt.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 5
+        amount: 1,
+        measurement: "Cup",
+        preparation: 20,
+      },
+      {
+        name: "Banana Yogurt",
+        type: "Natural Yogurt",
+        category: "Group 1",
+        src: "./assets/images/banana-yogurt.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Kiwi Yogurt", 
-            "type": "Natural Yogurt", 
-            "category": "Group 1", 
-            "src": "../assets/images/kiwi-yogurt.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 5
+        amount: 1,
+        measurement: "Cup",
+        preparation: 5,
+      },
+      {
+        name: "Kiwi Yogurt",
+        type: "Natural Yogurt",
+        category: "Group 1",
+        src: "./assets/images/kiwi-yogurt.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Fermented Milk", 
-            "type": "Fermented Milk", 
-            "category": "Group 1", 
-            "src": "../assets/images/fermented-milk.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 5
+        amount: 1,
+        measurement: "Cup",
+        preparation: 5,
+      },
+      {
+        name: "Fermented Milk",
+        type: "Fermented Milk",
+        category: "Group 1",
+        src: "./assets/images/fermented-milk.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Semi-Skimmed Milk", 
-            "type": "Milk", 
-            "category": "Group 1", 
-            "src": "../assets/images/semi-skimmed-milk.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Cup",
-            "preparation": 5
+        amount: 1,
+        measurement: "Cup",
+        preparation: 5,
+      },
+      {
+        name: "Semi-Skimmed Milk",
+        type: "Milk",
+        category: "Group 1",
+        src: "./assets/images/semi-skimmed-milk.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Integral Sandwich Bread", 
-            "type": "Bread", 
-            "category": "Group 2", 
-            "src": "../assets/images/integral-sandwich-bread.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 2,
-            "measurement": "Slices",
-            "preparation": 5
+        amount: 1,
+        measurement: "Cup",
+        preparation: 5,
+      },
+      {
+        name: "Integral Sandwich Bread",
+        type: "Bread",
+        category: "Group 2",
+        src: "./assets/images/integral-sandwich-bread.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Crepioca", 
-            "type": "Crepioca", 
-            "category": "Group 2", 
-            "src": "../assets/images/crepioca.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Disc",
-            "preparation": 5
+        amount: 2,
+        measurement: "Slices",
+        preparation: 5,
+      },
+      {
+        name: "Crepioca",
+        type: "Crepioca",
+        category: "Group 2",
+        src: "./assets/images/crepioca.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Banana Pancake", 
-            "type": "Pancake", 
-            "category": "Group 2", 
-            "src": "../assets/images/banana-pancake.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 2,
-            "measurement": "Discs",
-            "preparation": 5
+        amount: 1,
+        measurement: "Disc",
+        preparation: 5,
+      },
+      {
+        name: "Banana Pancake",
+        type: "Pancake",
+        category: "Group 2",
+        src: "./assets/images/banana-pancake.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "French Bread", 
-            "type": "Bread", 
-            "category": "Group 2", 
-            "src": "../assets/images/french-bread.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Unity",
-            "preparation": 1
+        amount: 2,
+        measurement: "Discs",
+        preparation: 5,
+      },
+      {
+        name: "French Bread",
+        type: "Bread",
+        category: "Group 2",
+        src: "./assets/images/french-bread.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Sandwich Bread", 
-            "type": "Bread", 
-            "category": "Group 2", 
-            "src": "../assets/images/sandwich-bread.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 2,
-            "measurement": "Slices",
-            "preparation": 1
+        amount: 1,
+        measurement: "Unity",
+        preparation: 1,
+      },
+      {
+        name: "Sandwich Bread",
+        type: "Bread",
+        category: "Group 2",
+        src: "./assets/images/sandwich-bread.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Granola", 
-            "type": "Granola", 
-            "category": "Group 2", 
-            "src": "../assets/images/granola.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 2,
-            "measurement": "Soup Spoons",
-            "preparation": 1
+        amount: 2,
+        measurement: "Slices",
+        preparation: 1,
+      },
+      {
+        name: "Granola",
+        type: "Granola",
+        category: "Group 2",
+        src: "./assets/images/granola.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Oat", 
-            "type": "Cereal", 
-            "category": "Group 2", 
-            "src": "../assets/images/oat.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 2,
-            "measurement": "Soup Spoons",
-            "preparation": 1
+        amount: 2,
+        measurement: "Soup Spoons",
+        preparation: 1,
+      },
+      {
+        name: "Oat",
+        type: "Cereal",
+        category: "Group 2",
+        src: "./assets/images/oat.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Cream Cheese", 
-            "type": "Cream", 
-            "category": "Group 3", 
-            "src": "../assets/images/cream-cheese.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Coffee Spoon",
-            "preparation": 1
+        amount: 2,
+        measurement: "Soup Spoons",
+        preparation: 1,
+      },
+      {
+        name: "Cream Cheese",
+        type: "Cream",
+        category: "Group 3",
+        src: "./assets/images/cream-cheese.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Ricotta Cream", 
-            "type": "Cream", 
-            "category": "Group 3", 
-            "src": "../assets/images/ricotta-cream.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Coffee Spoon",
-            "preparation": 1
+        amount: 1,
+        measurement: "Coffee Spoon",
+        preparation: 1,
+      },
+      {
+        name: "Ricotta Cream",
+        type: "Cream",
+        category: "Group 3",
+        src: "./assets/images/ricotta-cream.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Peanut Cream", 
-            "type": "Cream", 
-            "category": "Group 3", 
-            "src": "../assets/images/peanut-cream.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Coffee Spoon",
-            "preparation": 1
+        amount: 1,
+        measurement: "Coffee Spoon",
+        preparation: 1,
+      },
+      {
+        name: "Peanut Cream",
+        type: "Cream",
+        category: "Group 3",
+        src: "./assets/images/peanut-cream.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Jelly", 
-            "type": "Cream", 
-            "category": "Group 3", 
-            "src": "../assets/images/jelly.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Coffee Spoon",
-            "preparation": 1
+        amount: 1,
+        measurement: "Coffee Spoon",
+        preparation: 1,
+      },
+      {
+        name: "Jelly",
+        type: "Cream",
+        category: "Group 3",
+        src: "./assets/images/jelly.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Fruit", 
-            "type": "Fruit", 
-            "category": "Group 3", 
-            "src": "../assets/images/fruits.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Unity",
-            "preparation": 5
+        amount: 1,
+        measurement: "Coffee Spoon",
+        preparation: 1,
+      },
+      {
+        name: "Fruit",
+        type: "Fruit",
+        category: "Group 3",
+        src: "./assets/images/fruits.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Ricotta", 
-            "type": "Milk Derivative", 
-            "category": "Group 4", 
-            "src": "../assets/images/ricotta.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Slice",
-            "preparation": 5
+        amount: 1,
+        measurement: "Unity",
+        preparation: 5,
+      },
+      {
+        name: "Ricotta",
+        type: "Milk Derivative",
+        category: "Group 4",
+        src: "./assets/images/ricotta.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Mussarela Cheese", 
-            "type": "Milk Derivative", 
-            "category": "Group 4", 
-            "src": "../assets/images/mussarela-cheese.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Slice",
-            "preparation": 5
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Boiled Egg", 
-            "type": "Egg", 
-            "category": "Group 4", 
-            "src": "../assets/images/boiled-egg.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Unity",
-            "preparation": 5
+        amount: 1,
+        measurement: "Slice",
+        preparation: 5,
+      },
+      {
+        name: "Mussarela Cheese",
+        type: "Milk Derivative",
+        category: "Group 4",
+        src: "./assets/images/mussarela-cheese.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
         },
-        {
-            "name": "Mixed Egg", 
-            "type": "Egg", 
-            "category": "Group 4", 
-            "src": "../assets/images/mixed-eggs.jpg",
-            "recipe": {
-                "ingredients": [],
-                "steps": []
-            },
-            "amount": 1,
-            "measurement": "Unity",
-            "preparation": 5
-        }
+        amount: 1,
+        measurement: "Slice",
+        preparation: 5,
+      },
+      {
+        name: "Boiled Egg",
+        type: "Egg",
+        category: "Group 4",
+        src: "./assets/images/boiled-egg.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
+        },
+        amount: 1,
+        measurement: "Unity",
+        preparation: 5,
+      },
+      {
+        name: "Mixed Egg",
+        type: "Egg",
+        category: "Group 4",
+        src: "./assets/images/mixed-eggs.jpg",
+        recipe: {
+          ingredients: [],
+          steps: [],
+        },
+        amount: 1,
+        measurement: "Unity",
+        preparation: 5,
+      },
     ];
-    };
-    getSuggestions(){
-        return this.list;
+  }
+  getSuggestions() {
+    return this.list;
+  }
+  getMealGroup(item) {
+    for (let index = 0; index < this.list.length; index++) {
+      if (this.list[index].name == item) {
+        return this.list[index].category;
+      }
     }
-    getMealGroup(item){
-        for (let index = 0; index < this.list.length; index++) {
-            if (this.list[index].name == item) {
-                return this.list[index].category;
-            }
-        }
+  }
+  getMeal(item) {
+    for (let index = 0; index < this.list.length; index++) {
+      if (this.list[index].name == item) {
+        return this.list[index];
+      }
     }
-    getMeal(item){
-        for (let index = 0; index < this.list.length; index++) {
-            if (this.list[index].name == item) {
-                return this.list[index];
-            }
-        }
-    }
-    addMeal(item){
-        this.list.push(item);
-    }
-} 
+  }
+  addMeal(item) {
+    this.list.push(item);
+  }
+}
